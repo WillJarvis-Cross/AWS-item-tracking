a3af53fba1b593ffde8edf50f9ba86b5d1fa2498
WillJarvis-Cross <wjarviscross@gmail.com>
2024-11-09 15:23:39 -0500

    add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awsitemtracking1ad2e1e3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "awsitemtrackingGroupRole": "string"
+    }
+  }
+}
