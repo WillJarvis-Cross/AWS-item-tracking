eb103222b42a344234f5881d22f33c3965a11247
WillJarvis-Cross <wjarviscross@gmail.com>
2024-11-09 16:31:35 -0500

    update auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,16 +1,14 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "awsitemtracking1ad2e1e3": {
+    "awsitemtracking262e24b3": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
-    },
-    "userPoolGroups": {
-      "awsitemtrackingGroupRole": "string"
     }
   }
 }
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,17 +3,18 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:ff64261d-e38c-408a-8340-0e61d4b396a6",
+    "aws_cognito_identity_pool_id": "us-east-1:2a333aec-0b2b-4709-b2a5-696731bd2862",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_5l1wwspaq",
-    "aws_user_pools_web_client_id": "147f4od9ld27aehokvd0q5s2rf",
+    "aws_user_pools_id": "us-east-1_BKgvlsat7",
+    "aws_user_pools_web_client_id": "4h6farokr9n0erf0jo51047eic",
     "oauth": {},
     "aws_cognito_username_attributes": [
         "EMAIL"
     ],
     "aws_cognito_social_providers": [],
     "aws_cognito_signup_attributes": [
-        "EMAIL"
+        "EMAIL",
+        "PHONE_NUMBER"
     ],
     "aws_cognito_mfa_configuration": "OFF",
     "aws_cognito_mfa_types": [
